4779e9f89a0090a9222f5da33f4dde6c493930c0
Pedro Ferreira <111713739+KirinDev@users.noreply.github.com>
2022-10-04 16:19:39 +0100

    bot dev

--- main.js
+++ main.js
@@ -18,7 +18,12 @@ const thicc_images = [
   'thicc1.jpg', 'thicc2.jpg', 'thicc3.jpg', 'thicc4.jpg', 'thicc5.jpg', 'thicc6.jpg', 'thicc7.jpg', 'thicc8.jpg', 'thicc9.jpg', 'thicc10.jpg',
   'thicc11.jpg', 'thicc12.jpg', 'thicc13.jpg', 'thicc14.jpg' , 'thicc15.jpg' , 'thicc16.jpg' , 'thicc17.jpg' , 'thicc18.jpg' , 'thicc19.jpg' , 'thicc20.jpg'
 ];
-// path where the images are
+
+const ines_file = new AttachmentBuilder('./img_storage/unknown.png');
+const img_Ines = new EmbedBuilder()
+    .setImage('attachment://unknown.png')
+    .setColor(0x0099ff)
+    .setTitle('ofc its <@457225264888283137>')
 
 
 client.on('messageCreate' , (message) => {
@@ -26,6 +31,14 @@ client.on('messageCreate' , (message) => {
 
     const args = message.content.slice(prefix.length).split(/ +/)
     const command = args.shift().toLowerCase()
+
+    if ( command === 'hey <@1026610058995441764>') {
+        message.channel.send('sup bitch!')
+    }
+
+    if (message.content.includes('most') && message.content.includes('beautiful') ) {
+        message.channel.send({ embeds: [img_Ines] , files: [ines_file]})
+    }
 
     if ( command === 'peepo') {
         message.channel.send({ embeds: [peepo_img] })
